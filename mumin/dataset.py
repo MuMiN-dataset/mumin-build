1fb7709a18a595639e575823406d42c2dc691a70
saattrupdan <saattrupdan@gmail.com>
2021-08-31 15:19:27 +0100

    docs: Add MuMiNDataset docstring and repr

--- mumin/dataset.py
+++ mumin/dataset.py
@@ -15,11 +15,49 @@ logger = logging.getLogger(__name__)
 
 
 class MuMiNDataset:
-    '''MuMiN dataset'''
-    def __init__(self, dataset_dir: Union[str, Path] = '.'):
+    '''MuMiN dataset.
+
+    Args:
+        twitter_api_key (str):
+            The Twitter API key.
+        twitter_api_secret (str):
+            The Twitter API secret.
+        twitter_access_token (str):
+            The Twitter access token.
+        twitter_access_secret (str):
+            The Twitter access secret.
+        dataset_dir (str, optional):
+            The path to the folder where the dataset should be stored. Defaults
+            to the current directory.
+    '''
+    def __init__(self,
+                 twitter_api_key: str,
+                 twitter_api_secret: str,
+                 twitter_access_token: str,
+                 twitter_access_secret: str,
+                 dataset_dir: Union[str, Path] = '.'):
+        self.twitter_api_key = twitter_api_key
+        self.twitter_api_secret = twitter_api_secret
+        self.twitter_access_token = twitter_access_token
+        self.twitter_access_secret = twitter_access_secret
         self.dataset_dir = Path(dataset_dir)
-        self.nodes = None
-        self.relations = None
+        self.nodes: Dict[str, pd.DataFrame] = dict()
+        self.rels: List[Tuple[str, str, pd.DataFrame]] = list()
+
+    def __repr__(self) -> str:
+        '''A string representation of the dataaset.
+
+        Returns:
+            str: The representation of the dataset.
+        '''
+        if len(self.nodes) == 0 or len(self.rels) == 0:
+            return 'MuMiNDataset(compiled=False)'
+        else:
+            num_nodes = sum([len(df) for df in self.nodes.values()])
+            num_rels = sum([len(df) for _, _, df in self.rels])
+            return (f'MuMiNDataset(num_nodes={num_nodes:,}, '
+                    f'num_relations={num_rels:,}, '
+                    f'compiled=False)')
 
     def compile(self):
         '''Compiles the dataset.
