6ccf007960fb401fecc567b26ccad2cac59cea31
saattrupdan <saattrupdan@gmail.com>
2021-09-05 18:34:29 +0100

    chore: Ignore jieba logging

--- mumin/__init__.py
+++ mumin/__init__.py
@@ -6,5 +6,6 @@ fmt = '%(asctime)s [%(levelname)s - %(name)s] %(message)s'
 logging.basicConfig(level=logging.INFO, format=fmt)
 logging.getLogger('urllib3').setLevel(logging.CRITICAL)
 logging.getLogger('newspaper').setLevel(logging.CRITICAL)
+logging.getLogger('jieba').setLevel(logging.CRITICAL)
 
 from .dataset import MuminDataset  # noqa
