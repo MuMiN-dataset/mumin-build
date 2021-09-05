33bb59216a53efc985898def982a1b194e1976af
saattrupdan <saattrupdan@gmail.com>
2021-09-05 18:32:16 +0100

    chore: Ignore urllib3 and newspaper logging

--- mumin/__init__.py
+++ mumin/__init__.py
@@ -2,7 +2,9 @@ __version__ = '0.0.0'
 
 # Set up logging
 import logging
-fmt = '%(asctime)s [%(levelname)s] %(message)s'
+fmt = '%(asctime)s [%(levelname)s - %(name)s] %(message)s'
 logging.basicConfig(level=logging.INFO, format=fmt)
+logging.getLogger('urllib3').setLevel(logging.CRITICAL)
+logging.getLogger('newspaper').setLevel(logging.CRITICAL)
 
 from .dataset import MuminDataset  # noqa
