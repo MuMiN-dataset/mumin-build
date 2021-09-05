2a1dfa888b5071ed6e09569d010ce27f6387c532
saattrupdan <saattrupdan@gmail.com>
2021-09-05 19:17:55 +0100

    chore: Remove package name from logging

--- mumin/__init__.py
+++ mumin/__init__.py
@@ -2,7 +2,7 @@ __version__ = '0.0.0'
 
 # Set up logging
 import logging
-fmt = '%(asctime)s [%(levelname)s - %(name)s] %(message)s'
+fmt = '%(asctime)s [%(levelname)s] %(message)s'
 logging.basicConfig(level=logging.INFO, format=fmt)
 logging.getLogger('urllib3').disabled = True
 logging.getLogger('newspaper').disabled = True
