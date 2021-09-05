e7cd46ac81188668c47424a4c7f7d52b77df4170
saattrupdan <saattrupdan@gmail.com>
2021-09-05 18:17:38 +0100

    feat: Add logging setup in __init__

--- mumin/__init__.py
+++ mumin/__init__.py
@@ -1,3 +1,8 @@
 __version__ = '0.0.0'
 
+# Set up logging
+import logging
+fmt = '%(asctime)s [%(levelname)s] %(message)s'
+logging.basicConfig(level=logging.INFO, format=fmt)
+
 from .dataset import MuminDataset  # noqa
