ed0bc3a3cdffa7909cd22316d551aa2de8a43a4f
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2022-07-22 11:48:11 +0200

    feat: Move warning catching into download_article_with_timeout

--- src/mumin/article.py
+++ src/mumin/article.py
@@ -11,7 +11,9 @@ from wrapt_timeout_decorator.wrapt_timeout_decorator import timeout
 
 @timeout(10)
 def download_article_with_timeout(article: Article):
-    article.download()
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", category=DeprecationWarning)
+        article.download()
     return article
 
 
@@ -25,57 +27,53 @@ def process_article_url(url: str) -> Union[None, dict]:
         dict or None:
             The processed article, or None if the URL could not be parsed.
     """
-    # Ignore warnings while processing articles
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
+    # Remove GET arguments from the URL
+    stripped_url = re.sub(r'(\?.*"|\/$)', "", url)
 
-        # Remove GET arguments from the URL
-        stripped_url = re.sub(r'(\?.*"|\/$)', "", url)
+    try:
+        article = Article(stripped_url)
+        article = download_article_with_timeout(article)
+        article.parse()
+    except Exception:  # noqa
+        return None
 
-        try:
-            article = Article(stripped_url)
-            article = download_article_with_timeout(article)
-            article.parse()
-        except Exception:  # noqa
-            return None
+    # Extract the title and skip URL if it is empty
+    title = article.title
+    if title == "":
+        return None
+    else:
+        title = re.sub("\n+", "\n", title)
+        title = re.sub(" +", " ", title)
+        title = title.strip()
 
-        # Extract the title and skip URL if it is empty
-        title = article.title
-        if title == "":
-            return None
-        else:
-            title = re.sub("\n+", "\n", title)
-            title = re.sub(" +", " ", title)
-            title = title.strip()
+    # Extract the content and skip URL if it is empty
+    content = article.text.strip()
+    if content == "":
+        return None
+    else:
+        content = re.sub("\n+", "\n", content)
+        content = re.sub(" +", " ", content)
+        content = content.strip()
 
-        # Extract the content and skip URL if it is empty
-        content = article.text.strip()
-        if content == "":
-            return None
-        else:
-            content = re.sub("\n+", "\n", content)
-            content = re.sub(" +", " ", content)
-            content = content.strip()
+    # Extract the authors, the publishing date and the top image
+    authors = list(article.authors)
+    publish_date: Optional[str]
+    if article.publish_date is not None:
+        date = article.publish_date
+        publish_date = dt.datetime.strftime(date, "%Y-%m-%d")
+    else:
+        publish_date = None
+    try:
+        top_image_url = article.top_image
+    except AttributeError:
+        top_image_url = None
 
-        # Extract the authors, the publishing date and the top image
-        authors = list(article.authors)
-        publish_date: Optional[str]
-        if article.publish_date is not None:
-            date = article.publish_date
-            publish_date = dt.datetime.strftime(date, "%Y-%m-%d")
-        else:
-            publish_date = None
-        try:
-            top_image_url = article.top_image
-        except AttributeError:
-            top_image_url = None
-
-        data_dict = dict(
-            url=stripped_url,
-            title=title,
-            content=content,
-            authors=authors,
-            publish_date=publish_date,
-            top_image_url=top_image_url,
-        )
-        return data_dict
+    data_dict = dict(
+        url=stripped_url,
+        title=title,
+        content=content,
+        authors=authors,
+        publish_date=publish_date,
+        top_image_url=top_image_url,
+    )
+    return data_dict
